256dc715a69f9ec2da40f079afce47b15846b8d4
TonyAdo <coolhzb@163.com>
2014-09-28 16:43:44 +0800

    finish the ReadWriteStressTest part....

--- src/test/java/com/ctriposs/quickcache/QuickCacheReadWriteStressTest.java
+++ src/test/java/com/ctriposs/quickcache/QuickCacheReadWriteStressTest.java
@@ -1,15 +1,22 @@
 package com.ctriposs.quickcache;
 
+import com.ctriposs.quickcache.util.TestSample;
 import com.ctriposs.quickcache.util.TestUtil;
+import com.ctriposs.quickcache.utils.FileUtil;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -123,6 +130,75 @@ public class QuickCacheReadWriteStressTest {
         }
         for (String k : keys) {
             assertNull(cache.get(k));
+        }
+    }
+
+    @Test
+    public void testMultiThreadWriteTtlTwoMillion() throws Exception {
+        final long count = 2 * 1000 * 1000;
+        final int threadCount = 16;
+        ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        CacheConfig config = new CacheConfig();
+        config.setCapacityPerBlock(20 * 1024 * 1024)
+                .setExpireInterval(2 * 1000)
+                .setMigrateInterval(2 * 1000);
+        cache = new QuickCache<String>(TEST_DIR, config);
+
+        long start = System.nanoTime();
+        List<Future<?>> futures = new ArrayList<Future<?>>();
+        for (int i = 0; i < threadCount; i++) {
+            final int _i = i;
+            futures.add(service.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        final TestSample sample = new TestSample();
+                        StringBuilder sb = new StringBuilder();
+                        for (int j = _i; j < count; j+= threadCount) {
+                            sample.intA = j;
+                            sample.doubleA = j;
+                            sample.longA = j;
+                            cache.put(TestSample.users(sb, j), sample.toBytes(), 2 * 1000);
+                        }
+                        Thread.sleep(10 * 1000);
+                        for (int j = _i; j < count; j += threadCount) {
+                            assertNull(cache.get(TestSample.users(sb, j)));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }));
+        }
+
+        for (Future<?> future : futures) {
+            future.get();
+        }
+
+        long duration = System.nanoTime() - start;
+        System.out.printf("Put/get %dK operations per second%n", (int) (count * 4 * 1e6 / duration));
+
+        service.shutdown();
+    }
+
+    @After
+    public void close() throws Exception {
+        try {
+            cache.close();
+            FileUtil.deleteDirectory(new File(TEST_DIR));
+        } catch (IOException e) {
+            System.gc();
+            try {
+                FileUtil.deleteDirectory(new File(TEST_DIR));
+            } catch (IOException e1) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e2){/**/}
+                FileUtil.deleteDirectory(new File(TEST_DIR));
+            }
         }
     }
 }
