dce691403bcdcceb913fb57d25e253a4b7f622f8
TonyAdo <coolhzb@163.com>
2014-09-29 16:08:34 +0800

    update the remove dirty logic....

--- src/test/java/com/ctriposs/storage/StorageManagerUnitTest.java
+++ src/test/java/com/ctriposs/storage/StorageManagerUnitTest.java
@@ -84,12 +84,12 @@ public class StorageManagerUnitTest {
         assertTrue(5L == storageManager.getDirty());
         assertTrue((Head.HEAD_SIZE + 2 * Meta.META_SIZE) == pointer.getMetaOffset());
         assertTrue(testBytes.length == pointer.getValueSize());
-        assertTrue((2 * testBytes.length + smallTestBytes.length + 3 * keyBytes.length + 2 * Meta.META_SIZE + Head.HEAD_SIZE) == storageManager.getUsed());
+        assertTrue((2 * testBytes.length + smallTestBytes.length + 3 * keyBytes.length + 3 * Meta.META_SIZE + Head.HEAD_SIZE) == storageManager.getUsed());
 
         // remove
         resultBytes = storageManager.remove(pointer);
         assertEquals(testString, new String(resultBytes));
-        assertTrue(resultBytes.length == storageManager.getDirty());
+        assertTrue((Head.HEAD_SIZE + keyBytes.length + resultBytes.length + Meta.META_SIZE) == storageManager.getDirty());
 
         assertTrue(2 == storageManager.getTotalBlockCount());
         assertTrue(1 == storageManager.getFreeBlockCount());
